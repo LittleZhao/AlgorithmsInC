ab957f9cfae6555b8abe7a935c8f236b27ba6ac2
Zhao <LittleZhao@users.noreply.github.com>
2014-07-30 22:36:42 +0800

    heap sort

---
+++ C9_7.c
@@ -0,0 +1,78 @@
+//heap sort, low to high
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define exch(A, B) {int t=(A);(A)=(B);(B)=t;}
+#define L 10
+
+void heapsort(int *, int);
+void fixDown(int *, int, int);
+
+int main(void)
+{
+    int *arr = NULL, i;
+    srand((unsigned) time(NULL));
+
+    arr = (int *)malloc((L+1)*sizeof(int));
+
+    for (i=1; i<=L; i++)
+    {
+        arr[i] = rand() % 50;
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    heapsort(arr, L);
+
+    for (i=1; i<=L; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    free(arr);
+    arr = NULL;
+    return 0;
+}
+
+void heapsort(int *arr, int l)
+{
+    int k;
+
+    for (k=l/2; k>0; k--)
+    {
+        fixDown(arr, k, l);
+    }
+
+    while (l>1)
+    {
+        arr[1] ^= arr[l];
+        arr[l] ^= arr[1];
+        arr[1] ^= arr[l];
+        l--;
+        fixDown(arr, 1, l);
+    }
+    return;
+}
+
+void fixDown(int *arr, int f, int t)
+{
+    int i;
+    while (2*f<=t)
+    {
+        i = 2 * f;
+        if ((i<t) && (arr[i]<arr[i+1]))
+        {
+            i += 1;
+        }
+
+        if (arr[f]>=arr[i])
+        {
+            break;
+        }
+
+        exch(arr[f], arr[i]);
+        f = i;
+    }
+    return;
+
+}
