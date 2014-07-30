ef1105e273d51fe7694b5ae9840a75b84ccea409
Zhao <LittleZhao@users.noreply.github.com>
2014-07-30 22:35:07 +0800

    add the run command

--- C9_6.c
+++ C9_6.c
@@ -1,11 +1,9 @@
 //priority queue based on heap
+//command: cc C9_6.c C9_5.c C9_5.h
 #include <stdio.h>
 #include <time.h>
 #include "C9_5.h"
-#define L 5
-
-extern int *pq;
-extern int N;
+#define L 10
 
 int main(void)
 {
