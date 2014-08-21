c07b9a281fac794c3353c1122ca189e3eb442984
Zhao <LittleZhao@users.noreply.github.com>
2014-08-21 22:51:16 +0800

    first

---
+++ C4.2.c
@@ -0,0 +1,54 @@
+//cc C4.2.c stack.c STACK.h
+// when you use * in the command line, should be \*
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "STACK.h"
+#define N 20
+int main(int argc, char *argv[])
+{
+    char *e = NULL;
+    int i, j, len, temp;
+    STACKinit(argc-1);
+
+    for (j=1; j<argc; j++)
+    {
+        e = argv[j];
+        len = strlen(e);
+        for (i=0; i<len; i++)
+        {
+            if (e[i] =='+')
+            {
+                STACKpush(STACKpop() + STACKpop());
+            }
+
+            if (e[i] == '-')
+            {
+                STACKpush((-1) * (STACKpop() - STACKpop()));
+            }
+
+            if (e[i] == '*')
+            {
+                STACKpush(STACKpop() * STACKpop());
+            }
+
+            if (e[i]>='0' && e[i]<='9')
+            {
+                temp = 0;
+                while (e[i]>='0' && e[i]<='9')
+                {
+                    temp = temp*10 + (e[i]- '0');
+                    i++;
+                }
+                STACKpush(temp);
+            }
+
+        }
+    }
+
+    printf("%d\n", STACKpop());
+    STACKfree();
+    free(e);
+    e = NULL;
+    return 0;
+}
