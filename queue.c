bfed0d0dd23847e32fb8c3d22ca7c753336a8d48
Zhao <LittleZhao@users.noreply.github.com>
2014-08-28 22:38:25 +0800

    update for E4.33

--- queue.c
+++ queue.c
@@ -1,4 +1,5 @@
-#include <stalib.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "queue.h"
 
 typedef struct QUEUEnode *link;
@@ -10,11 +11,12 @@ struct QUEUEnode
     link next;
 };
 
-static link head, tail;
+static link head, tail, N;
 
 void QUEUEinit(int maxN)
 {
     head = NULL;
+    N = 0;
 }
 
 int QUEUEempty()
@@ -25,9 +27,29 @@ int QUEUEempty()
 static link NEW(int v, link next)
 {
     link n = (link)malloc(sizeof(*n));
+    if (NULL == n)
+    {
+        return NULL;
+    }
     n->data = v;
     n->next = next;
     return n;
+}
+
+static void QUEUEerror(int t)
+{
+    if (0 == t) //hard to test
+    {
+        printf("Put fails, FULL\n");
+        return;
+    }
+
+    if (1 == t)
+    {
+        printf("Get fails, EMPTY\n");
+        return;
+    }
+    return;
 }
 
 void QUEUEput(int value)
@@ -36,20 +58,38 @@ void QUEUEput(int value)
     {
         tail = NEW(value, head);
         head = tail;
+        if (NULL == head)
+        {
+            QUEUEerror(0);
+            return;
+        }
+
     }
     else
     {
         tail->next = NEW(value, tail->next);
+        if (NULL == tail->next)
+        {
+            QUEUEerror(0);
+            return;
+        }
         tail = tail->next;
     }
+    N++;
     return;
 }
 
 int QUEUEget()
 {
+    if (0 == N)
+    {
+        QUEUEerror(1);
+        exit(1);
+    }
     int value = head->data;
     link t = head;
     head = head->next;
     free(t);
+    N--;
     return value;
 }
